29bc8155d176d34a8f84cbaa926c744770d71869
sfsheng0322 <sfsheng0322@gmail.com>
2015-07-02 22:26:43 +0800

    add menu of PwdListActivity

--- app/src/main/java/com/smart/doorlock/ui/PwdListActivity.java
+++ app/src/main/java/com/smart/doorlock/ui/PwdListActivity.java
@@ -6,16 +6,17 @@ import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.Toast;
 
 import com.smart.doorlock.adapter.PwdAdapter;
 import com.smart.doorlock.entity.PwdEntity;
 import com.smart.doorlock.ui.base.BaseActivity;
 import com.smart.doorlock.util.DisplayUtil;
+import com.smart.doorlock.widget.swipemenulistview.SwipeMenu;
 import com.smart.doorlock.widget.swipemenulistview.SwipeMenuCreator;
 import com.smart.doorlock.widget.swipemenulistview.SwipeMenuItem;
 import com.smart.doorlock.widget.swipemenulistview.SwipeMenuListView;
 import com.smart.lock.R;
-import com.smart.doorlock.widget.swipemenulistview.SwipeMenu;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -56,21 +57,21 @@ public class PwdListActivity extends BaseActivity {
     private void initData() {
         pwdList = new ArrayList<>();
         pwdList.add(new PwdEntity("张三", "123456", "第一个密码"));
-        pwdList.add(new PwdEntity("李四", "123456", "第一个密码"));
-        pwdList.add(new PwdEntity("王五", "123456", "第一个密码"));
-        pwdList.add(new PwdEntity("小六", "123456", "第一个密码"));
-        pwdList.add(new PwdEntity("小七", "123456", "第一个密码"));
+        pwdList.add(new PwdEntity("李四", "123456", "第二个密码"));
+        pwdList.add(new PwdEntity("王五", "123456", "第三个密码"));
+        pwdList.add(new PwdEntity("小六", "123456", "第四个密码"));
+        pwdList.add(new PwdEntity("小七", "123456", "第五个密码"));
     }
 
     private void initView() {
-        initXlistView();
+        initSwipeMenuListView();
         pwdAdapter = new PwdAdapter(this, pwdList);
         smlvListView.setAdapter(pwdAdapter);
         setSwipeMenuCreator();
         initSwipeMenuItemClickListener();
     }
 
-    private void initXlistView() {
+    private void initSwipeMenuListView() {
         smlvListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
@@ -82,13 +83,22 @@ public class PwdListActivity extends BaseActivity {
         SwipeMenuCreator creator = new SwipeMenuCreator() {
             @Override
             public void create(SwipeMenu menu) {
-                SwipeMenuItem openItem = new SwipeMenuItem(getApplicationContext());
-                openItem.setBackground(new ColorDrawable(Color.rgb(0xF9, 0x3F, 0x25)));
-                openItem.setWidth(DisplayUtil.dip2px(PwdListActivity.this, 90));
-                openItem.setTitle("删除");
-                openItem.setTitleSize(16);
-                openItem.setTitleColor(Color.WHITE);
-                menu.addMenuItem(openItem);
+                SwipeMenuItem modifyItem = new SwipeMenuItem(getApplicationContext());
+                modifyItem.setBackground(new ColorDrawable(getResources().getColor(R.color.font_black_6)));
+                modifyItem.setWidth(DisplayUtil.dip2px(PwdListActivity.this, 90));
+                modifyItem.setTitle("修改");
+                modifyItem.setTitleSize(16);
+                modifyItem.setTitleColor(getResources().getColor(R.color.font_black_2));
+
+                SwipeMenuItem deleteItem = new SwipeMenuItem(getApplicationContext());
+                deleteItem.setBackground(new ColorDrawable(Color.rgb(0xF9, 0x3F, 0x25)));
+                deleteItem.setWidth(DisplayUtil.dip2px(PwdListActivity.this, 90));
+                deleteItem.setTitle("删除");
+                deleteItem.setTitleSize(16);
+                deleteItem.setTitleColor(Color.WHITE);
+
+                menu.addMenuItem(modifyItem);
+                menu.addMenuItem(deleteItem);
             }
         };
         smlvListView.setMenuCreator(creator);
@@ -100,6 +110,9 @@ public class PwdListActivity extends BaseActivity {
             public boolean onMenuItemClick(int position, SwipeMenu menu, int index) {
                 switch (index) {
                     case 0:
+                        Toast.makeText(PwdListActivity.this, "修改", Toast.LENGTH_SHORT).show();
+                        break;
+                    case 1:
                         deleteNewsListItem(position);
                         break;
                 }
